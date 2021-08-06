3eae0bd99e41cd5eefe5bdd8510bd02b2630390b
arianacabral <arianacabral57@gmail.com>
2021-08-06 16:51:54 -0300

    fim de aula

---
+++ caVetor2D/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caVetor2D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Usando o construtor padrão
+            Vetor v1 = new Vetor();
+
+            Console.WriteLine("Construtor padrão");
+            Console.WriteLine("Vetor: (" + v1.getX() + ", " + v1.getY()+") \n");
+
+            // Usando o construtor com argumetos
+            Vetor v2 = new Vetor(2.0,1.0);
+
+            Console.WriteLine("Construtor com argumentos");
+            Console.WriteLine("Vetor: (" + v2.getX() + ", " + v2.getY() + ") \n");
+
+            // Lendo valores do console
+            Console.Write("Entre com a coordenada X: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Entre com a coordenada Y: ");
+            double y = double.Parse(Console.ReadLine());
+            Vetor v3 = new Vetor(x, y);
+            Console.WriteLine("\nVetor informado pelo usuario: (" + v3.getX() + ", " + v3.getY() + ") \n");
+
+            // Apresentando no Console o modulo do vetor
+            Console.WriteLine("Módulo de (" + v3.getX() + "," + 
+                v3.getY() + "): " + v3.modulo() + " \n");
+
+            Console.WriteLine("Produto escalar entre (" + v2.getX() + "," + v2.getY() + 
+                ") e (" + v3.getX() + "," + v3.getY() + "): " + v2.prodctEscalarCom(v3) + "\n");
+
+            Console.WriteLine("Angulo entre (" + v2.getX() + "," + v2.getY() +
+                ") e (" + v3.getX() + "," + v3.getY() + "): " + v2.anguloCom(v3) + "º\n");
+
+            // Apresentando no Console a projecão de um vetor em outro
+            double[] proj = v2.projecaoEm(v3);
+
+            Console.WriteLine("Projeção de (" + v2.getX() + ", " + v2.getY() +
+                ") em (" + v3.getX() + ", " + v3.getY() + "): (" + proj[0].ToString() + 
+                ", " + proj[1].ToString() + ")" + "\n");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ caVetor2D/Vetor.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caVetor2D
+{
+    class Vetor
+    {
+        // membros da classe
+        private double m_x;
+        private double m_y;
+        double m_modulo;
+        double m_ang;
+        double[] m_proj = new double[2];
+
+        // construtor da classe
+        public Vetor()
+        {
+            m_x = 0.0;
+            m_y = 0.0; 
+        }
+
+        public Vetor(double x, double y)
+        {
+            m_x = x;
+            m_y = y; 
+        }
+
+        // getters
+        public double getX()
+        {
+            return(m_x);
+        }
+        public double getY()
+        {
+            return (m_y);
+        }
+
+        // setters
+        public void setX(double x)
+        {
+            m_x = x; 
+        }
+        public void setY(double y)
+        {
+            m_y = y;
+        }
+
+        // Função para cálculo do Módulo de um vetor 2D
+
+        public double modulo()
+        {
+            m_modulo = Math.Sqrt(Math.Pow(m_x,2) + Math.Pow(m_y,2));
+            return (m_modulo);
+        }
+
+        // Função para cálculo do Produto Escalar entre dois vetores 2D
+        public double prodctEscalarCom(Vetor v)
+        {
+            double prodct_esc = (m_x * v.m_x) + (m_y * v.m_y);
+            return (prodct_esc);
+        }
+  
+
+        // Função para cálculo do angulo entre dois vetores 2D
+        public double anguloCom(Vetor v)
+        {
+            m_ang = Math.Acos (prodctEscalarCom(v)/ (Math.Sqrt(Math.Pow(m_x, 2) + Math.Pow(m_y, 2))*v.modulo()));
+            m_ang = m_ang * (180 / Math.PI);
+            return (m_ang);
+        }
+
+        // Função para cálculo da projeção de um vetor em outro vetor
+        public double[] projecaoEm(Vetor v)
+        {
+            double prodct_esc = (m_x * v.m_x) + (m_y * v.m_y);
+            double proj_X = (prodct_esc/Math.Pow(v.modulo(), 2))*v.getX();
+            double proj_Y = (prodct_esc / Math.Pow(v.modulo(), 2)) * v.getY();
+            m_proj[0] = (prodct_esc / (Math.Pow(v.modulo(), 2))) * v.getX();
+            m_proj[1] = (prodct_esc / (Math.Pow(v.modulo(), 2))) * v.getY();
+      
+            return (m_proj);
+        }
+
+    }//fim da classe
+}
