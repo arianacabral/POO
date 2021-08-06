1a92486b6e14a6c6719c295af188b6d155c47315
arianacabral <arianacabral57@gmail.com>
2021-08-06 17:38:10 -0300

    fim de aula

---
+++ caTempo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caTempo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Construtor padrão
+            Tempo t1 = new Tempo();
+            Console.WriteLine("Construtor padrão");
+            Console.Write("Horário: ");
+            t1.imprimeTempo();
+
+            // Construtor com argumentos
+            Console.WriteLine("\nConstrutor com argumentos");
+            Console.Write("Horário: ");
+            Tempo t2 = new Tempo(13, 58, 23);
+            t2.imprimeTempo();
+
+            // Lendo valores do console
+            Console.WriteLine("\nLendo valores do console");
+            Tempo t3 = new Tempo();
+            t3.registraHorario();
+
+            // Apresentando horário informado pelo usuário 
+            Console.Write("\nHorário informado pelo usuário: ");
+            t3.imprimeTempo();
+
+            // Método para somar horários
+            Console.WriteLine("\nSomando Horários");
+            Console.Write("Horário: ");
+            t2.soma(t3).imprimeTempo();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ caTempo/Tempo.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caTempo
+{
+    class Tempo
+    {
+        //atributos da classe
+        private int hora;
+        private int min;
+        private int segs;
+
+        // construtor padrão
+        public Tempo() 
+        {
+            hora = 0;
+            min = 0;
+            segs = 0;
+        }
+
+        // construtor com argumentos 
+        public Tempo(int hora_, int min_, int segs_)
+        {
+            hora = hora_;
+            min = min_;
+            segs = segs_;
+        }
+
+        //getters
+        public int getHora()
+        {
+            return (hora); 
+        }
+
+        public int getMinuto()
+        {
+            return (min);
+        }
+
+        public int getSegundo()
+        {
+            return (segs);
+        }
+
+        // setters
+        public void setHora(int hora_)
+        {
+            hora = hora_;
+        }
+
+        public void setMinuto(int min_)
+        {
+            min = min_;
+        }
+
+        public void setSegundo(int segs_)
+        {
+            segs = segs_;
+        }
+
+        public void imprimeTempo()
+        {
+
+            if(segs < 60)
+            {
+                if(min < 60)
+                {
+                    if(hora <= 24)
+                    {
+                        Console.WriteLine(hora.ToString() + ":" + min.ToString() + ":" + segs.ToString());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Formato hora inválido!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Formato minuto inválido!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Fomato segundo inválido!");
+            }
+        }
+
+        // Método que registra o horário
+
+        public void registraHorario()
+        {
+            Console.Write("\nInforme as horas: ");
+            hora = int.Parse(Console.ReadLine());
+            Console.Write("Informe os minutos: ");
+            min = int.Parse(Console.ReadLine());
+            Console.Write("Informe os segundos: ");
+            segs = int.Parse(Console.ReadLine());
+        }
+
+        // Método que soma dos horários
+        public Tempo soma(Tempo t)
+        {
+            hora = hora + t.getHora();
+            min = min + t.getMinuto();
+            segs = segs + t.getSegundo();
+
+            if(segs >= 60)
+            {
+                min = min + (segs/60); // soma dos mins com o quociente da divisão dos segs por 60
+
+                segs = segs % 60; // resto da divisão dos segs por 60
+                
+                if(min >= 60)
+                {
+                    hora = hora + (min / 60);
+
+                    min = min % 60;
+                }
+            }
+            else
+            {
+                if (min >= 60)
+                {
+                    hora = hora + (min / 60);
+
+                    min = min % 60;
+                }
+            }
+
+            Tempo t_soma = new Tempo(hora,min,segs);
+
+            return (t_soma);
+        }
+
+    }// fim da classe
+}
