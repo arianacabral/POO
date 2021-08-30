1e368434feef045f1cb19247fe251effe7111d01
arianacabral <arianacabral57@gmail.com>
2021-08-30 11:17:43 -0300

    Add files via upload

---
+++ wfaControleDePagamentos/Pagtos.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wfaControleDePagamentos
+{
+    class Pagtos // classe mãe
+    {
+        //  atributos 
+        protected long m_cpf;
+        protected double m_valor;
+        protected int m_cod;
+
+        // construtor padrão
+        public Pagtos()
+        {
+            m_cpf = 000000000000;
+            m_valor = 0.0;
+            m_cod = 0;
+        }
+
+        // construtor com argumentos (aridade 3)
+        public Pagtos(long cpf, double valor, int cod)
+        {
+            m_cpf = cpf;
+            m_valor = valor;
+            m_cod = cod;
+        }
+
+        //  getters
+        public long getCPF()
+        {
+            return (m_cpf);
+        }
+
+        public double getValor()
+        {
+            return (m_valor);
+        }
+
+        public int getCod()
+        {
+            return (m_cod);
+        }
+
+        // setters
+        public void setCPF(long cpf)
+        {
+            m_cpf = cpf;
+        }
+
+        public void setValor(double valor)
+        {
+            m_valor = valor;
+        }
+
+        public void setCod(int cod)
+        {
+            m_cod = cod;
+        }
+
+        // método para gerar o valor da fatura 
+        public virtual void faturar()
+        {
+       
+        }
+
+    } // fim da classe
+}
---
+++ wfaControleDePagamentos/Saude.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wfaControleDePagamentos
+{
+    class Saude: Pagtos // classe filha
+    {
+        // atributos
+        private String m_estabelecimento;
+        private double m_vlfatSaude;
+
+        // construtor padrão
+        public Saude() : base()
+        {
+            m_estabelecimento = "";
+            m_vlfatSaude = 0.0;
+        }
+
+        // construtor com argumentos
+        public Saude(long cpf, double valor, int codigo, String estabelecimento) : base(cpf, valor, codigo)
+        {
+            m_estabelecimento = estabelecimento;
+            m_vlfatSaude = 0.0;
+        }
+
+        // getters
+        public String getEstabelecimento()
+        {
+            return (m_estabelecimento);
+        }
+
+        public double getValorFaturaSaude()
+        {
+            return (m_vlfatSaude);
+        }
+
+        // setters
+        public void setEstabelecimento(String estabelecimento)
+        {
+            m_estabelecimento = estabelecimento;
+        }
+
+        public void setValorFaturaSaude(double valorFaturaSaude)
+        {
+            m_vlfatSaude = valorFaturaSaude;
+        }
+
+        // método para gerar o valor da fatura 
+        public override void faturar()
+        {
+            m_vlfatSaude = Math.Round(m_valor + (0.12 * m_valor), 2);
+
+        }
+
+    } // fim da classe
+}
