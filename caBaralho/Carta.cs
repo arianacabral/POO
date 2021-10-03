a815dc827e02969866d32be107ddb1a68c19cf8a
arianacabral <arianacabral57@gmail.com>
2021-10-03 18:02:32 -0300

    fim de aula

---
+++ caBaralho/Carta.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caBaralho
+{
+    class Carta
+    {
+        // atributos da classe
+        private String m_face;
+        private String m_naipe;
+        private Random m_rnd;
+        private int m_num;
+        private Carta m_carta;
+
+        // construtor padrão
+        public Carta()
+        {
+            m_face = "";
+            m_naipe = "";
+
+        }
+
+        // construtor com argumentos
+        public Carta(String face, String naipe)
+        {
+            m_face = face;
+            m_naipe = naipe;
+
+        }
+
+        // getters
+        public String getFace()
+        {
+            return (m_face);
+        }
+
+        public String getNaipe()
+        {
+            return (m_naipe);
+        }
+
+        // setters
+        public void setFace(String face)
+        {
+            m_face = face;
+        }
+
+        public void setNaipe(String naipe)
+        {
+            m_naipe = naipe;
+        }
+
+        // método para gerar a Face da carta
+        public String gerarFace()
+        {
+            m_rnd = new Random();
+
+            m_num = m_rnd.Next(1, 14);
+
+
+            if (m_num >= 2 & m_num <= 10)
+            {
+                m_face = m_num.ToString();
+            }
+            else
+            {
+                switch (m_num)
+                {
+                    case 1:
+                        m_face = "A";
+                        break;
+                    case 11:
+                        m_face = "J";
+                        break;
+                    case 12:
+                        m_face = "Q";
+                        break;
+                    case 13:
+                        m_face = "K";
+                        break;
+                }
+            }
+
+            return (m_face);
+
+        }
+
+        // método para gerar Naipe da carta
+        public String gerarNaipe()
+        {
+            m_rnd = new Random();
+
+            m_num = m_rnd.Next(1, 5);
+            switch (m_num)
+            {
+                case 1:
+                    m_naipe = "PAUS"; 
+                    break;
+                case 2:
+                    m_naipe = "COPAS";
+                    break;
+                case 3:
+                    m_naipe = "ESPADAS";
+                    break;
+                default:
+                    m_naipe = "OURO";
+                    break;
+            }
+
+            return (m_naipe);
+
+        }
+
+        public Carta gerarCarta()
+        {
+            m_carta = new Carta();
+
+            m_carta.setFace(gerarFace());
+            m_carta.setNaipe(gerarNaipe());
+
+            return (m_carta);
+        }
+    }
+}
---
+++ caBaralho/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caBaralho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Baralho
+            Console.WriteLine("\t\tBaralho");
+
+            Queue<Carta> baralho = new Queue<Carta>();
+
+            Carta carta;
+
+            /// MENU
+
+            int aux = 1;
+
+            while (aux != 0)
+            {
+                Console.Clear();
+                Console.WriteLine("\t\tMENU\n");
+                Console.WriteLine(" [1] - Número de cartas na mão");
+                Console.WriteLine(" [2] - Pegar carta na mesa");
+                Console.WriteLine(" [3] - Descartar primeira carta da mão na mesa");
+                Console.WriteLine(" [4] - Ver cartas na mão");
+                Console.WriteLine(" [5] - Sair");
+                Console.WriteLine("---------------------------------------\n");
+
+                Console.Write("Digite a opção do MENU desejada: ");
+                string op = Console.ReadLine();
+
+                switch (op)
+                {
+                    case "1":
+
+                        Console.Clear();
+
+                        Console.WriteLine("\nNúmero de cartas na mão: " + baralho.Count);
+
+                        Console.ReadLine();
+
+                        break;
+
+                    case "2":
+
+                        Console.Clear();
+
+                        carta = new Carta();
+
+                        carta.gerarCarta();
+
+                        baralho.Enqueue(carta);
+
+                        Console.WriteLine("\n Carta adicionada à mão: " + carta.getFace() + " - " + carta.getNaipe());
+
+                        Console.ReadLine();
+
+                        break;
+                    case "3":
+
+                        Console.Clear();
+
+                        if(baralho.Count > 0)
+                        {
+                            Carta descarte = baralho.Dequeue();
+
+                            Console.WriteLine("\nCarta lançada à mesa: " + descarte.getFace() + " - " + descarte.getNaipe());
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nAdicionar carta à mão!");
+                        }
+
+                        Console.ReadLine();
+
+                        break;
+                    case "4":
+
+                        Console.Clear();
+
+                        Console.WriteLine("\nCartas na mão \n");
+
+                        foreach(Carta carta_mao in baralho)
+                        {
+                            //string f = carta_mao.getFace();
+                            //string c = carta_mao.getNaipe();
+                            //Console.WriteLine(f + " - " + c);
+
+                            Console.WriteLine(carta_mao.getFace() + " - " + carta_mao.getNaipe());
+                        }
+
+                        Console.ReadLine();
+
+                        break;
+                    case "5":
+
+                        aux = 0; // sair do while
+
+                        break;
+
+                    default:
+
+                        Console.Clear();
+
+                        Console.Write("\nFavor, selecionar uma das opções do MENU!\n\n");
+
+                        break;
+
+                } // fim do switch
+
+            } 
+        }
+    }
+}
